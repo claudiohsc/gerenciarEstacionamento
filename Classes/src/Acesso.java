ac811fe2b7c0c77c6b9512959b8397785c332c2f
CLaudio Henrique <claudio885henrique@gmail.com>
2023-06-24 22:12:11 -0300

    corrigindo classes

---
+++ Classes/src/Acesso.java
@@ -0,0 +1,5 @@
+package gerenciaEstacionamento;
+
+public class Acesso {
+
+}
---
+++ Classes/src/Veiculo.java
@@ -0,0 +1,20 @@
+package gerenciaEstacionamento;
+
+public class Veiculo {
+
+	private String placa;
+	private String modelo;
+	
+	private sdf horaEntrada;
+	private sdf horaSaida;
+	
+	private float valorAPagar;
+	
+	public void entrarEstacionamento() {
+		
+	}
+	
+	public void sairEstacionamento() {
+		
+	}
+}
