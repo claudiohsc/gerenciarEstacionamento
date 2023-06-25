2478b6c77cbd082583231e785c5d2d0bd18a0f30
CLaudio Henrique <claudio885henrique@gmail.com>
2023-06-24 23:42:46 -0300

    adicionando classe Veiculo

--- Classes/src/Veiculo.java
+++ Classes/src/Veiculo.java
@@ -1,9 +1,7 @@
 package gerenciaEstacionamento;
 
 import java.util.Date;
-import java.text.DateFormat;
-import java.util.Locale;
-
+import java.text.SimpleDateFormat;
 
 
 public class Veiculo {
@@ -11,24 +9,86 @@ public class Veiculo {
 	private String placa;
 	private String modelo;
 	
-	private Date hoje = new Date();
 	
-	//Locale.setDefault()
+	SimpleDateFormat formatarDataHora = new SimpleDateFormat("dd/M/yy hh:mm:ss");
 	
-	String hojeFormat = DateFormat.getInstance().format(hoje);
+	private String hoje = formatarDataHora.format(new Date());
 	
-	
-	
-	private Date horaEntrada;
-	private Date horaSaida;
-	
-	private float valorAPagar;
-	
-	public void entrarEstacionamento() {
 		
+	private Boolean verificacao = false;
+	
+	private String horaEntrada; // "dd/mm/yy hh:mm:ss"
+	private String horaSaida;  // "dd/mm/yy hh:mm:ss"
+	
+	private float valorAPagar; // xx.xx
+	
+	public Veiculo(String placa, String modelo, String horaEntrada, String horaSaida, Boolean verificacao) {
+		super();
+		this.placa = placa;
+		this.modelo = modelo;
+		this.horaEntrada = horaEntrada;
+		this.horaSaida = horaSaida;
+		this.verificacao = verificacao;
+	}
+
+
+	public void entrarEstacionamento() {
+		if(this.verificacao == false) {
+			this.verificacao = true;
+			System.out.println("O veículo entrou no estacionamento!");
+		}
+		else {
+			System.out.println("O veículo já está dentro do estacionamento!");
+		}
 	}
 	
 	public void sairEstacionamento() {
-		
+		if(this.verificacao == true) {
+			this.verificacao = false;
+			System.out.println("O veículo saiu do estacionamento!");
+		}
+		else {
+			System.out.println("O veículo já saiu do estacionamento!");
+		}
+	}
+	
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getHoraEntrada() {
+		return horaEntrada;
+	}
+
+	public void setHoraEntrada(String horaEntrada) {
+		this.horaEntrada = horaEntrada;
+	}
+
+	public String getHoraSaida() {
+		return horaSaida;
+	}
+
+	public void setHoraSaida(String horaSaida) {
+		this.horaSaida = horaSaida;
+	}
+
+	public float getValorAPagar() {
+		return valorAPagar;
+	}
+
+	public void setValorAPagar(float valorAPagar) {
+		this.valorAPagar = valorAPagar;
 	}
 }
