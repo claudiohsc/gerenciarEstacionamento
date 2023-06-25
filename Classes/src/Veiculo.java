c0ff44c0893cb4ca7a528206554f6fa370e05e15
CLaudio Henrique <claudio885henrique@gmail.com>
2023-06-24 22:06:59 -0300

    corrigindo classes

---
+++ Classes/src/Veiculo.java
@@ -0,0 +1,20 @@
+package gerenciaEstacionamento;
+
+public class Veiculo {
+
+	private String placa;
+	private String modelo;
+	
+	private DateTime horaEntrada;
+	private DateTime horaSaida;
+	
+	private float valorAPagar;
+	
+	public void entrarEstacionamento() {
+		
+	}
+	
+	public void sairEstacionamento() {
+		
+	}
+}
