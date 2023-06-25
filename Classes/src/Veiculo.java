fd102328ab569c359d4c0dcb3b0011a4b0619cfb
CLaudio Henrique <claudio885henrique@gmail.com>
2023-06-24 22:57:52 -0300

    add veiculo

--- Classes/src/Veiculo.java
+++ Classes/src/Veiculo.java
@@ -1,12 +1,26 @@
 package gerenciaEstacionamento;
 
+import java.util.Date;
+import java.text.DateFormat;
+import java.util.Locale;
+
+
+
 public class Veiculo {
 
 	private String placa;
 	private String modelo;
 	
-	private sdf horaEntrada;
-	private sdf horaSaida;
+	private Date hoje = new Date();
+	
+	//Locale.setDefault()
+	
+	String hojeFormat = DateFormat.getInstance().format(hoje);
+	
+	
+	
+	private Date horaEntrada;
+	private Date horaSaida;
 	
 	private float valorAPagar;
 	
